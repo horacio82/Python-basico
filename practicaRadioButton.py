52a79e2001c431cdee13bf0f48fceeae4f22a24f
Horacio Molinari <molinarih2011@gmail.com>
2023-10-31 20:20:49 -0300

    Nuevos

---
+++ practicaRadioButton.py
@@ -0,0 +1,21 @@
+#Curso Python. Interfaces gráficas IX. Vídeo 50
+from tkinter import*
+
+root=Tk()
+varOpcion=IntVar()
+
+def imprimir():
+    #print(varOpcion.get())
+    if varOpcion.get()==1:
+        etiqueta.config(text="Has elegido masculino")
+    else:
+        etiqueta.config(text="Has elegido femenino")    
+
+Label(root,text="Género:").pack()
+Radiobutton(root,text="Masculino",variable=varOpcion,value=1,command=imprimir).pack()
+Radiobutton(root,text="Femenino",variable=varOpcion,value=2,command=imprimir).pack()
+
+etiqueta=Label(root)
+etiqueta.pack()
+
+root.mainloop()
