65393db41354d091097b13b41fe4d370f24f0117
Horacio Molinari <molinarih2011@gmail.com>
2023-11-01 20:05:57 -0300

    Nuevos

--- Ejemplo1BBDD.py
+++ Ejemplo1BBDD.py
@@ -3,7 +3,14 @@ import sqlite3
 miConexion=sqlite3.connect("Primera base")
 miCursor=miConexion.cursor()
 #miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER,SECCION VARCHAR(20))")
-miCursor.execute("INSERT INTO PRODUCTOS VALUES('BALÓN',15,'DEPORTES')")
+#miCursor.execute("INSERT INTO PRODUCTOS VALUES('BALÓN',15,'DEPORTES')")
+
+miCursor.execute("SELECT * FROM PRODUCTOS")
+
+variosProductos=miCursor.fetchall()
+
+for producto in variosProductos:
+    print("Nombre artículo:",producto[0],"Sección:",producto[2])
 
 miConexion.commit()
 
