60f8cc1fedbacac7be63e2a693887430938ff09e
Horacio Molinari <molinarih2011@gmail.com>
2023-11-04 20:14:15 -0300

    nuevos

---
+++ Ejemplo2BBDD.py
@@ -0,0 +1,27 @@
+#Píldoras informáticas - video 57
+import sqlite3
+
+miConexion=sqlite3.connect("GestionProductos")
+miCursor=miConexion.cursor()
+
+miCursor.execute('''
+    CREATE TABLE PRODUCTOS(
+    CODIGO_ARTICULO VARCHAR(4) PRIMARY KEY,
+    NOMBRE_ARTICULO VARCHAR(50),
+    PRECIO INTEGER,
+    SECCION VARCHAR(20))
+''')
+
+productos=[
+    ("AR01", "pelota", 20, "Juguetería"),
+    ("AR02", "pantalón", 15, "confección"),
+    ("AR03", "destornillador", 25, "ferretería"),
+    ("AR04", "Jarrón", 45, "cerámica")
+]
+
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?,?)", productos)
+
+
+
+miConexion.commit()
+miConexion.close() 
